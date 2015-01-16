fb3bef94fc13c7e319c9e5a1a12b4bad8af10747
michael nauman <bodhiandmike@gmail.com>
2015-01-16 11:33:44 -0800

    refactored number 3 and cleaned up number 5

--- app.js
+++ app.js
@@ -2,17 +2,14 @@
 
 // michael nauman
 
-
-// Your code here
 function main() {
-	changeTitle();
-	colorYellow();
+  changeTitle();
+  colorYellow();
 
-	// initialize all Event Listeners
-    document.getElementById("essentials").addEventListener("click", changeClass);
-	document.querySelector("img").addEventListener("click", changeImage);
-	document.getElementById("reset").addEventListener("click", myReset);
-
+  // initialize all Event Listeners
+  document.getElementById("essentials").addEventListener("click", changeClass);
+  document.querySelector("img").addEventListener("click", changeImage);
+  document.getElementById("reset").addEventListener("click", myReset);
 }
 
 // 1. change the greeting from "Hello, World!" to "Hello, Planet Earth!".
@@ -30,25 +27,25 @@ function colorYellow () {
 
 // 3. add the class of selected to an <li> when it is clicked.
 function changeClass(){
-	if ("LI" == event.target.tagName){
-		event.target.className += "selected";
-	}
+  event.target.setAttribute("class", "selected");
+  document.querySelector("img").setAttribute("src", "images/" + event.target.innerHTML + ".jpeg");
 }
 
 // 4. change the image to be the last clicked food item.
 function changeImage() {
-		var imageName = event.target.innerHTML;
-		var picPath = "./images/" + imageName + ".jpeg";
-		document.querySelector("img").setAttribute("src", picPath);
+  var imageName = event.target.innerHTML;
+  var picPath = "./images/" + imageName + ".jpeg";
+  document.querySelector("img").setAttribute("src", picPath);
 }
 
 // 5. clicking the reset button should remove the selected class from each 
-//    <li> and change the image to panic.jpeg.
+//  <li> and change the image to panic.jpeg.
 function myReset () {
-	var reset = document.getElementById("reset");
-	console.log("push buttons");
-	document.querySelector("img").setAttribute("src", "./images/panic.jpeg");
+  var reset = document.getElementById("reset");
+  console.log("push buttons");
+  document.querySelector("img").setAttribute("src", "./images/panic.jpeg");
 
+// TODO: remove the strike through for all the LI's
 }
 
 
