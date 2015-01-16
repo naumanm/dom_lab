5489e6672a5027e709bfabbe92aa7081e7c44187
michael nauman <bodhiandmike@gmail.com>
2015-01-15 17:23:30 -0800

    answered number2

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // Your code here
+function main() {
+	changeTitle();
+	colorYellow();
+}
+
+// 1. change the greeting from "Hello, World!" to "Hello, Planet Earth!".
+function changeTitle() {
+  var Title = document.getElementById("greeting");
+  Title.innerHTML = "My hello";
+}
+
+// 2. set the backgroundColor of each <li> to yellow.
+function colorYellow () {
+  var liS = document.getElementsByTagName("li");  
+  for(var i = 0; i < liS.length; i++) {
+  	var liSItem = liS[i];
+  	console.log(liSItem);
+    liSItem.style.background = "yellow";	
+  }
+}
+
+// 3. add the class of selected to an <li> when it is clicked.
+
+
+// 4. change the image to be the last clicked food item.
+
+
+// 5. clicking the reset button should remove the selected class from each 
+//    <li> and change the image to panic.jpeg.
+
+
+// when the DOM is loaded, cal
+window.onload = main;
